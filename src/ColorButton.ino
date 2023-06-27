a6fe89cdc045988e4f3e6c60d407b51d0793f5b7
Fenton Pratt <fadpratt@gmail.com>
2023-06-27 13:46:08 -0500

    initial commit

---
+++ src/ColorButton.ino
@@ -0,0 +1,15 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+void setup() {
+  pinMode(A5, INPUT);
+  pinMode(D0, INPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
