be1a183f2618bb52998d7a2a2028efdb3ed2c6bf
Fenton Pratt <fadpratt@gmail.com>
2023-06-27 14:38:34 -0500

    Color Changer Final Commit

---
+++ src/ColorButton.cpp
@@ -0,0 +1,52 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/admin/Documents/CTD_2023/Personal_Projects/ColorButton/src/ColorButton.ino"
+void setup();
+void loop();
+#line 1 "/Users/admin/Documents/CTD_2023/Personal_Projects/ColorButton/src/ColorButton.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+void setup() {
+  pinMode(A5, INPUT);
+  pinMode(D0, INPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  digitalWrite(D4, HIGH);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
+  Serial.begin(9600);
+}
+bool cont = false;
+bool onOff = false;
+int red = 0;
+int green = 0;
+int blue = 0;
+int current = 0;
+void loop() {
+  if(current==0) {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D4, red);
+  } else if(current==1) {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D5, red);
+  } else {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D6, red);
+  }
+  if(digitalRead(D0)==HIGH&&!cont) {
+    Serial.println("Button Down");
+    onOff = !onOff;
+    current+=1;
+    cont=true;
+  }
+  if(digitalRead(D0)==LOW) {
+    cont = false;
+  }
+}
--- src/ColorButton.ino
+++ src/ColorButton.ino
@@ -6,10 +6,38 @@ void setup() {
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
+  digitalWrite(D4, HIGH);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
+  Serial.begin(9600);
 }
-
-// loop() runs over and over again, as quickly as it can execute.
+bool cont = false;
+bool onOff = false;
+int red = 0;
+int green = 0;
+int blue = 0;
+int current = 0;
 void loop() {
-  // The core of your code will likely live here.
-
+  if(current==0) {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D4, red);
+  } else if(current==1) {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D5, red);
+  } else {
+    int analog = analogRead(A5);
+    red = round((analog*255/4095));
+    analogWrite(D6, red);
+  }
+  if(digitalRead(D0)==HIGH&&!cont) {
+    Serial.println("Button Down");
+    onOff = !onOff;
+    current+=1;
+    cont=true;
+  }
+  if(digitalRead(D0)==LOW) {
+    cont = false;
+  }
 }
