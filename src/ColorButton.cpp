d1832154b5b0a1ddc1984aa5e9fc56de06c22b41
Fenton Pratt <fadpratt@gmail.com>
2023-06-27 17:17:40 -0500

    kind of works

--- src/ColorButton.cpp
+++ src/ColorButton.cpp
@@ -21,6 +21,9 @@ void setup() {
   digitalWrite(D6, HIGH);
   Serial.begin(9600);
 }
+
+const int colorCount = 3;
+
 bool cont = false;
 bool onOff = false;
 bool cont2 = false;
@@ -30,35 +33,37 @@ int green = 0;
 int blue = 0;
 int current = 0;
 int current2 = 0;
-int colors[5][3];
+int colors[colorCount][3];
 bool cont3 = true;
+bool edit = false;
+bool edit2 = true;
 void loop() {
-  if(current==0) {
+  if(current==0&&edit2) {
     int analog = analogRead(A5);
     red = round((analog*255/4095));
     analogWrite(D4, red);
-    if(!cont3) {
+    if(!cont3&&edit) {
       colors[current2][0] = red;
     }
-  } else if(current==1) {
+  } else if(current==1&&edit2) {
     int analog = analogRead(A5);
     green = round((analog*255/4095));
     analogWrite(D5, green);
-    if(!cont3) {
+    if(!cont3&&edit) {
       colors[current2][1] = green;
     }
-  } else {
+  } else if(edit2) {
     int analog = analogRead(A5);
     blue = round((analog*255/4095));
     analogWrite(D6, blue);
-    if(!cont3) {
+    if(!cont3&&edit) {
       colors[current2][2] = blue;
     }
   }
   if(digitalRead(D0)==HIGH&&!cont) {
     onOff = !onOff;
     current+=1;
-    if(current==4) {
+    if(current==3) {
       current = 0;
     }
     cont=true;
@@ -67,22 +72,33 @@ void loop() {
     cont = false;
   }
   if(digitalRead(D1)==HIGH&&!cont2) {
+    red = 0;
+    green = 0;
+    blue = 0;
     onOff2 = !onOff2;
     current2+=1;
+    current = 0;
     cont2=true;
-    if(current2==5) {
+    if(current2==colorCount) {
       cont3 = false;
+      edit2 = false;
       current2 = 0;
     }
     if(cont3) {
       colors[current2][0] = red;
       colors[current2][1] = green;
       colors[current2][2] = blue;
+      digitalWrite(D4, HIGH);
+      digitalWrite(D5, HIGH);
+      digitalWrite(D6, HIGH);
       current = 0;
     } else {
       red = colors[current2][0];
       green = colors[current2][1];
       blue = colors[current2][2];
+      analogWrite(D4, red);
+      analogWrite(D5, green);
+      analogWrite(D6, blue);
     }
   }
   if(digitalRead(D1)==LOW) {
